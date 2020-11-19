fa86fedf039eea465b9818d98388561676ed5b11
Your Name <you@example.com>
2020-11-19 14:44:50 +0900

    deploy

---
+++ bookmark/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.urls import reverse
+# Create your models here.
+# 모델 : DB를 SQL없이 다루려고 모델을 사용
+# 우리가 데이터를 객체화해서 다루겠다.
+# 모델 = DB 테이블
+# 모델의 필드 = 테이블의 컬럼
+# 인스턴스 = 테이블의 레코드
+# 필드의 값(인스턴스의 필드값) = 레코드의 컬럼 데이터값
+class Bookmark(models.Model):
+    site_name = models.CharField(max_length=100)
+    url = models.URLField('Site URL')
+    # 필드의 종류가 결정하는 것
+    # 1. 데이터베이스의 컬럼 종류
+    # 2. 제약 사항 (몇글자까지 등)
+    # 3. Form의 종류
+    # 4. Form에서 제약 사항
+
+    def __str__(self):
+        return "이름 : " + self.site_name+", 주소 : "+self.url
+
+    def get_absolute_url(self):
+        return reverse('detail', args=[self.id])
+
+# 모델을 만들었다 => 데이터베이스에 어떤 데이터들을 어떤 형태로 넣으지 결정
+# 마이그레이션(migrate) => 데이터베이스에 모델의 내용을 반영(테이블 생성)
+# 모델을 수정할 경우 마이그레이션을 또 해줘야한다.
+# makemigrations => 모델의 변경사항을 추적해서 기록
